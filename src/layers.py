f93980b3bab36c1563bf6e3ee9f2a2d4ac861e6f
Johan Edstedt <johed950@student.liu.se>
2020-02-13 13:01:29 +0100

    changed variable names for clarity

--- src/layers.py
+++ src/layers.py
@@ -212,10 +212,10 @@ class CapsuleCONV(nn.Module):
             )        
     def input_expansion(self, input):
         # input has size [batch x num_of_capsule x height x width x  x capsule_dimension]
-        tiled_input = input.unfold(2,size=self.kernel_size,step=self.stride).unfold(3,size=self.kernel_size,step=self.stride)
-        tiled_input = res.permute([0,1,5,6,2,3,4])
+        unfolded_input = input.unfold(2,size=self.kernel_size,step=self.stride).unfold(3,size=self.kernel_size,step=self.stride)
+        unfolded_input = res.permute([0,1,5,6,2,3,4])
         # output has size [batch x num_of_capsule x kernel_size x kernel_size x h_out x w_out x capsule_dimension]
-        return tiled_input
+        return unfolded_input
     
     def forward(self, input, num_iter, next_capsule_value=None):
         # k,l: kernel size
